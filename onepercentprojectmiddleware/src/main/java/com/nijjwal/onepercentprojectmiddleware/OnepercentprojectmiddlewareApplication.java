1f0227b0f8b0bfa2b03e2ab855c2f5ddea493ca6
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2020-11-14 18:15:51 -0700

    OPAD-1: Initial Commit for empty Spring Boot project.

---
+++ onepercentprojectmiddleware/src/main/java/com/nijjwal/onepercentprojectmiddleware/OnepercentprojectmiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.nijjwal.onepercentprojectmiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepercentprojectmiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepercentprojectmiddlewareApplication.class, args);
+	}
+
+}
