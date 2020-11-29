20377ea5702b0a89416523ee48b850ca9917b65c
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2020-11-29 00:25:37 -0700

    OPAD-4: Added a page and created a service to add new customer to the application.

--- onepercentprojectfrontend/src/components/App.js
+++ onepercentprojectfrontend/src/components/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import CustomersPage from '../pages/CustomersPage';
 import SearchACustomerPage from '../pages/SearchACustomerPage';
+import AddCustomerPage from '../pages/AddCustomerPage';
 
 function App() {
 
@@ -13,13 +14,15 @@ function App() {
             <li><i className="fa fa-fw fa-home"></i><Link to="/">Home</Link></li>
             <li><i className="fa fa-fw fa-envelope"></i><Link to="/customer">Customers</Link></li>
             <li><i className="fa fa-fw fa-search"></i><Link to="/search">Search</Link></li>
+            <li><i className="fa fa-user-plus"></i><Link to="/addcustomer">Add Customer</Link></li>
           </ul>
         </nav>
 
         <Switch>
-          <Route path="/customer" component={CustomersPage}/>
-          <Route path="/search" component={SearchACustomerPage}/>
-          <Route path="/" component={HomePage}/>
+          <Route path="/customer" component={CustomersPage} />
+          <Route path="/search" component={SearchACustomerPage} />
+          <Route path="/addcustomer" component={AddCustomerPage} />
+          <Route path="/" component={HomePage} />
         </Switch>
 
       </Router>
