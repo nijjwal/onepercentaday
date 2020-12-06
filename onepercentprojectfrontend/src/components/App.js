4baf988574899da16c112f344e242f3d72b4b204
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2020-12-05 18:00:34 -0700

    OPAD-6: Added fron-end code to get new data for exisitng user

--- onepercentprojectfrontend/src/components/App.js
+++ onepercentprojectfrontend/src/components/App.js
@@ -3,6 +3,7 @@ import HomePage from '../pages/HomePage';
 import CustomersPage from '../pages/CustomersPage';
 import SearchACustomerPage from '../pages/SearchACustomerPage';
 import AddCustomerPage from '../pages/AddCustomerPage';
+import UpdateCustomerPage from '../pages/UpdateCustomerPage';
 
 function App() {
 
@@ -12,16 +13,18 @@ function App() {
         <nav>
           <ul className="horizontalNavBar">
             <li><i className="fa fa-fw fa-home"></i><Link to="/">Home</Link></li>
-            <li><i className="fa fa-fw fa-envelope"></i><Link to="/customer">Customers</Link></li>
+            <li><i className="fa fa-users"></i><Link to="/customer">Customers</Link></li>
             <li><i className="fa fa-fw fa-search"></i><Link to="/search">Search</Link></li>
-            <li><i className="fa fa-user-plus"></i><Link to="/addcustomer">Add Customer</Link></li>
+            <li><i className="fa fa-user-plus"></i><Link to="/addCustomer">Add Customer</Link></li>
+            <li><i className="fas fa-user-edit"></i><Link to="/updateCustomer">Update Customer</Link></li>
           </ul>
         </nav>
 
         <Switch>
           <Route path="/customer" component={CustomersPage} />
           <Route path="/search" component={SearchACustomerPage} />
-          <Route path="/addcustomer" component={AddCustomerPage} />
+          <Route path="/addCustomer" component={AddCustomerPage} />
+          <Route path="/updateCustomer" component={UpdateCustomerPage} />
           <Route path="/" component={HomePage} />
         </Switch>
 
---
+++ onepercentprojectfrontend/src/pages/UpdateCustomerPage.js
@@ -0,0 +1,13 @@
+import UpdateCustomerService from '../services/UpdateCustomerService';
+
+
+function UpdateCustomerPage(){
+    return (
+        <div>
+           <UpdateCustomerService/>
+        </div>
+    );
+
+}
+
+export default UpdateCustomerPage;
