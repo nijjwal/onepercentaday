5ff784e332dcc323f87c45d1e95b3c83e2661cbe
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2020-12-05 19:07:24 -0700

    OPAD-7: Added a page and service to allow users to delete profile

--- onepercentprojectfrontend/src/components/App.js
+++ onepercentprojectfrontend/src/components/App.js
@@ -4,6 +4,7 @@ import CustomersPage from '../pages/CustomersPage';
 import SearchACustomerPage from '../pages/SearchACustomerPage';
 import AddCustomerPage from '../pages/AddCustomerPage';
 import UpdateCustomerPage from '../pages/UpdateCustomerPage';
+import DeleteCustomerPage from '../pages/DeleteCustomerPage';
 
 function App() {
 
@@ -15,16 +16,18 @@ function App() {
             <li><i className="fa fa-fw fa-home"></i><Link to="/">Home</Link></li>
             <li><i className="fa fa-users"></i><Link to="/customer">Customers</Link></li>
             <li><i className="fa fa-fw fa-search"></i><Link to="/search">Search</Link></li>
-            <li><i className="fa fa-user-plus"></i><Link to="/addCustomer">Add Customer</Link></li>
-            <li><i className="fas fa-user-edit"></i><Link to="/updateCustomer">Update Customer</Link></li>
+            <li><i className="fa fa-user-plus"></i><Link to="/addcustomer">Add Customer</Link></li>
+            <li><i className="fas fa-user-edit"></i><Link to="/updatecustomer">Update Customer</Link></li>
+            <li><i className="fas fa-user-times"></i><Link to="/deletecustomer">Delete Customer</Link></li>
           </ul>
         </nav>
 
         <Switch>
           <Route path="/customer" component={CustomersPage} />
           <Route path="/search" component={SearchACustomerPage} />
-          <Route path="/addCustomer" component={AddCustomerPage} />
-          <Route path="/updateCustomer" component={UpdateCustomerPage} />
+          <Route path="/addcustomer" component={AddCustomerPage} />
+          <Route path="/updatecustomer" component={UpdateCustomerPage} />
+          <Route path="/deletecustomer" component={DeleteCustomerPage} />
           <Route path="/" component={HomePage} />
         </Switch>
 
